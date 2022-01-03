3b1397604e3e01bc782a3179e8810375ef6bd67a
Rachael Dawn <rdawn@cisco.com>
2022-01-03 11:19:03 -0700

    Added some java stuff

--- init.vim
+++ init.vim
@@ -4,4 +4,5 @@ filetype off
 lua require 'plugins'
 lua require 'settings'
 lua require 'shortcuts'
+lua require 'treesitter'
 
---
+++ lua/treesitter.lua
@@ -0,0 +1,8 @@
+sitter = require('nvim-treesitter.configs')
+
+sitter.setup {
+  ensure_installed = "maintained",
+  hightlight = {
+    enable = true
+  }
+}
